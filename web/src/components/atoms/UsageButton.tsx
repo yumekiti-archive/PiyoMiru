8c82d4c008987629af510c2d8b5d00ea3d9c1e93
yumekiti <yumekiti120440@gmail.com>
2022-10-21 20:57:03 +0900

    `add` usage page

--- web/src/components/atoms/UsageButton.tsx
+++ web/src/components/atoms/UsageButton.tsx
@@ -1,15 +1,23 @@
 import { FC } from 'react';
+import Parents from '../../assets/parents.svg';
+import Driver from '../../assets/driver.svg';
 
-const UsageButton: FC = () => {
+interface Props {
+  text: string;
+  driver: boolean;
+  onClick?: () => void;
+}
+
+const UsageButton: FC<Props> = ({ text, driver, onClick }) => {
   return (
-    <button className='w-full h-44 bg-[#FFFAE3] rounded-2xl'>
-      <div className='h-1/2 flex justify-center items-center'>
-        保護者様は
+    <button className='w-full h-44 bg-[#FFFAE3] rounded-2xl' onClick={onClick}>
+      <div className='h-1/2 flex justify-center items-center pt-4'>
+        {text}
         <br />
         こちら
       </div>
       <div className='h-1/2 flex justify-center items-end'>
-        <div className='w-20 h-20 rounded-xl bg-gray-400'></div>
+        <img src={driver ? Driver : Parents} alt='icon' />
       </div>
     </button>
   );
--- web/src/pages/UsagePage.tsx
+++ web/src/pages/UsagePage.tsx
@@ -5,20 +5,23 @@ import UsageButton from '../components/atoms/UsageButton';
 
 const UsagePage: FC = () => {
   return (
-    <div className='bg-[#FBD579] h-screen w-full'>
-      <div className='h-2/6 w-full flex items-end justify-center'>
+    <div className='h-screen bg-[#FBD579] w-full'>
+      <div className='h-1/6' />
+      <div className='h-1/6 w-full flex items-center justify-center'>
         <img src={NameLogo} alt='logo' className='w-72' />
       </div>
-      <div className='h-3/6 w-full flex justify-evenly items-start'>
+      <div className='h-2/6 w-full flex justify-evenly items-start'>
         <div className='w-4/12'>
-          <UsageButton />
+          <UsageButton text='保護者様は' driver={false} />
         </div>
         <div className='w-4/12'>
-          <UsageButton />
+          <UsageButton text='運転手の方は' driver={true} />
         </div>
       </div>
-      <div className='h-1/6 w-10/12 mx-auto'>
-        <FormButton text='ログイン' />
+      <div className='h-2/6 w-full flex items-center justify-center'>
+        <div className='w-10/12'>
+          <FormButton text='決定' />
+        </div>
       </div>
     </div>
   );
