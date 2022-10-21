66cc53de18c3c3d3996f24066f2dee01b2c9c0ac
yumekiti <yumekiti120440@gmail.com>
2022-10-21 09:58:27 +0900

    `refactoring`

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -4,13 +4,15 @@ import Logo from '../assets/loadingLogo.svg';
 
 const Loading: FC = () => {
   return (
-    <div className='loading flex h-screen z-50 bg-[#FBD579] fixed top-0 left-0 w-full items-center flex-col'>
-      <div className='w-full h-1/4 flex items-end justify-center'>
+    <div className="loading bg-[#FBD579] h-screen w-full fixed top-0 left-0">
+      <div className='h-1/6' />
+      <div className='h-1/6 w-full flex items-startz justify-center'>
         <img src={Logo} alt='logo' className='w-72' />
       </div>
-      <div className='h-3/4 w-full flex items-center justify-center'>
+      <div className='h-3/6 w-full flex justify-center items-center'>
         <img src={Hiyoko} alt='hiyoko' className='w-40 h-40' />
       </div>
+      <div className='h-1/6' />
     </div>
   );
 };
