0cef4d3c1f162c0e0617c7078c6b3681c1f70f52
yumekiti <yumekiti120440@gmail.com>
2022-10-21 11:55:53 +0900

    `refactoring`

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -4,15 +4,16 @@ import Logo from '../assets/loadingLogo.svg';
 
 const Loading: FC = () => {
   return (
-    <div className="loading bg-[#FBD579] h-screen w-full fixed top-0 left-0">
+    <div className="loading bg-[#FBD579] h-screen w-full fixed top-0 left-0 z-50">
       <div className='h-1/6' />
-      <div className='h-1/6 w-full flex items-startz justify-center'>
+      <div className='h-1/6 w-full flex items-center justify-center'>
         <img src={Logo} alt='logo' className='w-72' />
       </div>
-      <div className='h-3/6 w-full flex justify-center items-center'>
-        <img src={Hiyoko} alt='hiyoko' className='w-40 h-40' />
-      </div>
       <div className='h-1/6' />
+      <div className='h-1/6 w-full flex justify-center items-center'>
+        <img src={Hiyoko} alt='hiyoko' className='pt-4 h-full' />
+      </div>
+      <div className='h-2/6' />
     </div>
   );
 };
--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -14,16 +14,23 @@ const Login: FC = () => {
   };
 
   return (
-    <div className='flex h-screen bg-[#FBD579] items-center justify-center fixed top-0 left-0 w-full flex-col'>
-      <div className='h-2/6 w-full flex items-end justify-center'>
+    <div className='h-screen bg-[#FBD579] w-full'>
+      <div className='h-1/6' />
+      <div className='h-1/6 w-full flex items-center justify-center'>
         <img src={NameLogo} alt='logo' className='w-72' />
       </div>
-      <div className='h-3/6 w-10/12 flex justify-start flex-col items-center space-y-8 pt-10'>
-        <FormText label='ユーザーネーム' value={email} onChange={(e) => setEmail(e.target.value)} />
-        <FormText label='パスワード' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <div className='h-2/6 flex justify-start flex-col items-center space-y-8 pt-10'>
+        <div className='w-10/12 flex justify-center'>
+          <FormText label='パスワード' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className='w-10/12 flex justify-center'>
+          <FormText label='ユーザーネーム' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
       </div>
-      <div className='h-1/6 w-10/12 flex items-start justify-center'>
-        <FormButton text='ログイン' onClick={login} />
+      <div className='h-2/6 w-full flex items-center justify-center'>
+        <div className='w-10/12'>
+          <FormButton text='ログイン' onClick={login} />
+        </div>
       </div>
     </div>
   );
