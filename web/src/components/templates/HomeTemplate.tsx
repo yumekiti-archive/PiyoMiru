adb4a61af016fa8f4a5f221c2f185ad2685a6b94
yumekiti <yumekiti120440@gmail.com>
2023-01-15 01:42:03 +0900

    `add` AddBus

--- web/src/components/templates/HomeTemplate.tsx
+++ web/src/components/templates/HomeTemplate.tsx
@@ -1,10 +1,8 @@
-import { FC, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { FC } from 'react';
 
 import Header from '../organisms/Header';
 import Background from '../organisms/Background';
-import FormText from '../atoms/FormText';
+import AddBus from '../organisms/AddBus';
 import BusButton from '../atoms/BusButton';
 
 import Pluss from '../../assets/button/pluss.svg';
@@ -26,29 +24,13 @@ const HomeTemplate: FC<Props> = ({ addBusFlag, busName, setBusName, setAddBusFla
         <Background type='home' />
         <Header title='' />
         {addBusFlag && (
-          <div className='fixed top-0 left-0 w-full h-full z-50 bg-black bg-opacity-50 flex justify-center items-center'>
-            <div className='w-5/6 h-2/6 bg-white rounded-xl flex justify-start items-center border-2 border-[#FBD579] flex-col'>
-              <div className='h-2/6 flex justify-center items-center'>
-                <p className='text-xl'>バスの名前を入力してください</p>
-              </div>
-              <div className='h-2/6 w-5/6 flex justify-center items-center'>
-                <FormText value={busName} onChange={(e) => setBusName(e.target.value)} />
-              </div>
-              <div className='h-2/6 w-full flex justify-center items-center'>
-                <button
-                  className=' h-12 w-4/12 bg-[#E3EEF9] text-[#666666] rounded-xl mr-16'
-                  onClick={() => {
-                    setAddBusFlag(!addBusFlag);
-                  }}
-                >
-                  キャンセル
-                </button>
-                <button className=' h-12 w-4/12 bg-[#ED6D47] text-white rounded-xl' onClick={addBus}>
-                  追加
-                </button>
-              </div>
-            </div>
-          </div>
+          <AddBus
+            addBusFlag={addBusFlag}
+            busName={busName}
+            setBusName={setBusName}
+            setAddBusFlag={setAddBusFlag}
+            addBus={addBus}
+          />
         )}
         <div className='h-screen w-full'>
           <div className='h-1/6' />
