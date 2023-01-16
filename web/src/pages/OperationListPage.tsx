633316b628d0bf7a100ea73fe667fbd00ec24e7b
yumekiti <yumekiti120440@gmail.com>
2023-01-17 03:16:06 +0900

    `add` Stop button

--- web/src/pages/OperationListPage.tsx
+++ web/src/pages/OperationListPage.tsx
@@ -4,6 +4,8 @@ import { useQuery } from 'react-query';
 
 import Header from '../components/organisms/Header';
 import ListCard from '../components/atoms/ListCard';
+import EmphasisButton from '../components/atoms/EmphasisButton';
+
 import { usePassengersFind } from '../hooks/passengers';
 
 const OperationListPage: FC = () => {
@@ -13,11 +15,15 @@ const OperationListPage: FC = () => {
 
   if (data && !passengers) setPassengers(data);
 
+  const Stop = () => {
+    console.log('運転終了');
+  };
+
   return (
     passengers && (
       <>
         <Header title='乗車中園児 一覧' />
-        <div className='mt-32 w-full flex items-center justify-center pb-4 flex-col space-y-4'>
+        <div className='mt-32 w-full h-screen flex items-center justify-start mb-40 flex-col space-y-4'>
           {passengers.map((passenger: any) => (
             <ListCard
               key={passenger.attributes.users_permissions_user.data.id}
@@ -25,6 +31,19 @@ const OperationListPage: FC = () => {
               createdAt={passenger.attributes.createdAt}
             />
           ))}
+        </div>
+        <div className='w-full sticky bottom-10 flex items-center justify-center'>
+          <div className='w-8/12 h-16 flex items-center justify-center'>
+            <EmphasisButton
+              text='運転終了'
+              onClick={Stop}
+              mainBgColor='bg-[#90D7EC]'
+              subBgColor='bg-[#6EC5CA]'
+              color='text-[#666666]'
+              size='text-4xl'
+              top='top-4'
+            />
+          </div>
         </div>
       </>
     )
