9d27f30fa508e83e2be3e5f57b103a7dfbbe9a0d
yumekiti <yumekiti120440@gmail.com>
2023-01-17 12:17:32 +0900

    `fix` client

--- web/src/libs/client.ts
+++ web/src/libs/client.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const fetch = axios.create({
-  baseURL: `http://${window.location.hostname}`,
+  baseURL: `${window.location.origin}`,
 });
 
 export const fetchWithOauth = (jwt: any) =>
   axios.create({
-    baseURL: `http://${window.location.hostname}`,
+    baseURL: `${window.location.origin}`,
     headers: {
       Authorization: `Bearer ${jwt}`,
     },
