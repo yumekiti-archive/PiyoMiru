c14a1600145c44f12d7cf8f4caf4fad26af17079
yumekiti <yumekiti120440@gmail.com>
2023-01-19 14:48:40 +0900

    `add` capacitor

---
+++ web/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'piyomiru.yumekiti.net',
+  appName: 'PiyoMiru',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
