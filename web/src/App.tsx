0741c1d88e3e8adbe5891cbf4f0f0d4b66833d8a
yumekiti <yumekiti120440@gmail.com>
2023-01-22 06:17:22 +0900

    `add` Notification

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from 'react-query';
 import Router from './router';
 import socket from './socket';
@@ -6,11 +6,30 @@ import socket from './socket';
 const queryClient = new QueryClient();
 
 const App: FC = () => {
+  useEffect(() => {
+    if ("Notification" in window) {
+      // 通知が許可されていたら早期リターン
+      const permission = Notification.permission;
+      if (permission === "denied" || permission === "granted") {
+        return;
+      }
+      // 通知の許可を求める
+      Notification.requestPermission();
+    }
+  }, []);
+
+  const handlePushNotif = () => {
+    if ("Notification" in window) {
+      const notif = new Notification("バスに動きがありました");
+    }
+  };
+
   socket.on('connect', () => {
     console.log('Connected to serve');
   });
 
   socket.on('refresh', () => {
+    handlePushNotif();
     queryClient.invalidateQueries('me');
     queryClient.invalidateQueries('user');
     queryClient.invalidateQueries('bus');
