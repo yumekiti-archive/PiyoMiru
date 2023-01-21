9af48bd33aa071cc76306e6dc7e02d1e8008cd38
yumekiti <yumekiti120440@gmail.com>
2023-01-21 22:35:03 +0900

    `add` grayscale

--- web/src/components/atoms/ListCard.tsx
+++ web/src/components/atoms/ListCard.tsx
@@ -5,13 +5,14 @@ import Icon from '../../assets/icon.svg';
 interface Props {
   name: string;
   createdAt: string;
+  status?: boolean;
 }
 
-const ListCard: FC<Props> = ({ name, createdAt }) => {
+const ListCard: FC<Props> = ({ name, status, createdAt }) => {
   const date = new Date(createdAt);
 
   return (
-    <div className='w-11/12 h-24 bg-white rounded-xl border-2 border-[#FBD579] flex items-center justify-start'>
+    <div className='w-11/12 h-24 bg-white rounded-xl border-2 border-[#FBD579] flex items-center justify-start relative'>
       <div className='ml-2 w-1/5 rounded-xl h-4/6 flex items-center justify-center bg-[#D9D9D9]'>
         <img src={Icon} alt='icon' className='w-20 h-20' />
       </div>
@@ -19,6 +20,8 @@ const ListCard: FC<Props> = ({ name, createdAt }) => {
         <div className='text-3xl w-full h-1/2 flex items-center justify-start'>{name} さん</div>
         <div className='w-full h-1/2 flex items-center justify-start'>{date.toLocaleString()}</div>
       </div>
+      {/* statusがfalseならグレースケール */}
+      {!status && <div className='absolute w-full h-full bg-[#D9D9D9] opacity-50' />}
     </div>
   );
 };
