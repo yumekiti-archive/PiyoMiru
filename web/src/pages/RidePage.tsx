4a67890245ff9e2e34ef44712ef895f31118671b
yumekiti <yumekiti120440@gmail.com>
2023-01-21 22:35:37 +0900

    `fix` navigate

--- web/src/pages/RidePage.tsx
+++ web/src/pages/RidePage.tsx
@@ -11,17 +11,13 @@ const RidePage: FC = () => {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
 
-  const [flag, setFlag] = useState(false);
-
   const busId = localStorage.getItem('busId');
   if (busId === null) return <Navigate to='/' />;
 
   const { data: user } = useQuery('user', () => useUsersFindOne(id).then((res) => res.data));
   const { data: operations } = useQuery('operations', () => useOperationsFind(busId).then((res) => res.data));
 
-  if (!flag && user && operations) {
-    setFlag(true);
-
+  if (user && operations) {
     usePassengersFindMyId(id, operations.data[0].id).then((res) => {
       // ない場合
       if (!(res.data.data.length > 0)) {
@@ -35,14 +31,15 @@ const RidePage: FC = () => {
         });
         // ある場合
       } else {
-        if (!res.data.data[0].attributes.status) return;
-        usePassengersUpdate(res.data.data[0].id, {
-          data: {
-            status: false,
-          },
-        }).then((res) => {
-          console.log('update', res);
-        });
+        if (res.data.data[0].attributes.status) {
+          usePassengersUpdate(res.data.data[0].id, {
+            data: {
+              status: false,
+            },
+          }).then((res) => {
+            console.log('update', res);
+          });
+        }
       }
 
       navigate(`/list/${operations.data[0].id}`);
