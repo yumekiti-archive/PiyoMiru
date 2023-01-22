aa81b77abe3fdd862360701f91ec4d5502b4ffd0
yumekiti <yumekiti120440@gmail.com>
2023-01-22 21:28:29 +0900

    `fix` whitespace

--- web/src/components/atoms/Button.tsx
+++ web/src/components/atoms/Button.tsx
@@ -15,7 +15,7 @@ const Button: FC<Props> = ({ text, onClick, bgColor, color, size }) => {
       ${color ? color : 'text-[#666666]'}
       ${bgColor ? bgColor : 'bg-[#FFFF88]'}
       ${size ? size : 'text-2xl'}
-      w-full h-full rounded-2xl
+      w-full h-full rounded-2xl whitespace-nowrap
     `}
       onClick={onClick}
     >
