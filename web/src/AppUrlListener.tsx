bc388083084a106d2d19945885773ec209afcfeb
yumekiti <yumekiti120440@gmail.com>
2023-01-24 18:41:26 +0900

    `fix` deep link

--- web/src/AppUrlListener.tsx
+++ web/src/AppUrlListener.tsx
@@ -8,7 +8,7 @@ const AppUrlListener: React.FC<any> = () => {
     App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
       // Example url: https://beerswift.app/tabs/tab2
       // slug = /tabs/tab2
-      const slug = event.url.split('.app').pop();
+      const slug = event.url.split('piyomiru:/').pop();
       if (slug) {
         navigate(slug);
       }
