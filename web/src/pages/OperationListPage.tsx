4dc20c20e1466db5b3ef953e686df2c26f8b6f17
yumekiti <yumekiti120440@gmail.com>
2023-01-26 14:56:04 +0900

    `fix` relative

--- web/src/pages/OperationListPage.tsx
+++ web/src/pages/OperationListPage.tsx
@@ -43,7 +43,7 @@ const OperationListPage: FC = () => {
     passengers &&
     me && (
       <>
-        <div className='h-full w-full relative'>
+        <div className='h-full w-full'>
           <div className='w-full flex items-center justify-center'>
             <Header title='乗車中園児 一覧' />
           </div>
